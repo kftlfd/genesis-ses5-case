222a031ac2ea9c425ac2800efd35d937c116f1e3
kftlfd <kftlfd@ex.ua>
2025-05-17 18:57:21 +0300

    reset db migrations

--- drizzle/0000_even_wong.sql
+++ drizzle/0000_even_wong.sql
@@ -1,5 +1,5 @@
 CREATE TYPE "public"."frequency" AS ENUM('daily', 'hourly');--> statement-breakpoint
-CREATE TABLE "subscriptions" (
+CREATE TABLE "gses5_subscriptions" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"email" varchar(255) NOT NULL,
 	"city" varchar(255) NOT NULL,
@@ -7,5 +7,5 @@ CREATE TABLE "subscriptions" (
 	"confirmed" boolean DEFAULT false NOT NULL,
 	"confirmToken" varchar(255) NOT NULL,
 	"unsubToken" varchar(255) NOT NULL,
-	CONSTRAINT "subscriptions_email_unique" UNIQUE("email")
+	CONSTRAINT "gses5_subscriptions_email_unique" UNIQUE("email")
 );
