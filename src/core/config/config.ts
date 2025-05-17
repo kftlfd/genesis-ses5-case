760b0fdbf7dc538dd74fd18beadcf5754c4e74a6
kftlfd <kftlfd@ex.ua>
2025-05-17 17:18:06 +0300

    update docker setup

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
+  HOST: z.string().default('localhost'),
+  PORT: z.coerce.number().default(8000),
   DB_URL: z.string(),
   WEATHER_API_URL: z.string().url(),
   WEATHER_API_KEY: z.string(),
@@ -13,9 +15,11 @@ export class AppConfig {
 
   constructor() {
     this.env = envSchema.parse({
+      HOST: process.env.HOST,
+      PORT: process.env.PORT,
       DB_URL: process.env.DB_URL,
       WEATHER_API_URL: process.env.WEATHER_API_URL,
       WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-    } satisfies Partial<EnvConfig>);
+    } satisfies Record<keyof EnvConfig, string | undefined>);
   }
 }
