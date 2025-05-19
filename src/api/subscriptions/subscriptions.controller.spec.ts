279b2ddf75df88efa7deb59dc9993c757ea070d8
kftlfd <kftlfd@ex.ua>
2025-05-19 18:13:16 +0300

    update tests

--- src/api/subscriptions/subscriptions.controller.spec.ts
+++ src/api/subscriptions/subscriptions.controller.spec.ts
@@ -5,6 +5,8 @@ import { EmailModule } from '@/email/email.module';
 
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
+import { CreateSubDto } from './dto/create-sub.dto';
+import { Response } from 'express';
 
 describe('SubscriptionController', () => {
   let controller: SubscriptionsController;
@@ -21,5 +23,25 @@ describe('SubscriptionController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create-sub', () => {
+    it('should error on invalid input', async () => {
+      await expect(
+        controller.createSubscription({} as CreateSubDto, {} as Response),
+      ).rejects.toThrow();
+    });
+  });
+
+  describe('confirm-sub', () => {
+    it('should error on invalid input', async () => {
+      await expect(controller.confirmSub('inv')).rejects.toThrow();
+    });
+  });
+
+  describe('remove-sub', () => {
+    it('should error on invalid input', async () => {
+      await expect(controller.removeSub('inv')).rejects.toThrow();
+    });
   });
 });
